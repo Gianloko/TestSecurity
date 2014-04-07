375f3e547c2f837c66125f8eb527e4fb9a65d935
Gianluca Tessitore <gian.tessitore@gmail.com>
2014-04-07 22:23:21 +0200

    Mod

---
+++ src/main/java/SimplifiedJSPServlet.java
@@ -0,0 +1,31 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+/**
+ *
+ * @author Giangio
+ */
+public class SimplifiedJSPServlet extends HttpServlet{
+    
+    private static final long serialVersionUID = 1L;
+    
+    void mergeScriptlets(
+    
+            HttpServletRequest request,
+            HttpServletResponse response,
+            HttpSession session,
+            ServletContext application,
+            JspWriter out,
+            ServletConfig config,
+            JspContext jspContext,
+            Object page,
+            PageContext pageContext,
+            Throwable exception
+            
+    )throws Throwable{
+    
+        String test;
+    
+    }
+    
+}
